d9e48f4154729777eb513a0edbd3c231b533c5a3
ChaosLiu <1150180619@qq.com>
2024-04-27 22:27:29 +0800

    feat: complete exercise conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -7,6 +7,8 @@
 // Execute `rustlings hint from_into` or use the `hint` watch subcommand for a
 // hint.
 
+use std::usize;
+
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -40,10 +42,23 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            Default::default()
+        }
+
+        match s.split_once(",") {
+            Some((name, _)) if name.is_empty() => Default::default(),
+            Some((name, age)) => match age.parse::<usize>() {
+                Ok(age) => Person {
+                    name: name.to_string(),
+                    age: age,
+                },
+                Err(_) => Default::default(),
+            },
+            None => Default::default(),
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -11,6 +11,7 @@
 
 use std::num::ParseIntError;
 use std::str::FromStr;
+use std::{error, usize, vec};
 
 #[derive(Debug, PartialEq)]
 struct Person {
@@ -31,8 +32,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +51,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let splited_str: Vec<&str> = s.split(",").collect();
+
+        match &splited_str[..] {
+            &[name, _] if name.is_empty() => Err(ParsePersonError::NoName),
+            &[name, age_str] => match age_str.parse::<usize>() {
+                Ok(age) => Ok(Person {
+                    name: name.to_string(),
+                    age,
+                }),
+                Err(e) => Err(ParsePersonError::ParseInt(e)),
+            },
+            _ => Err(ParsePersonError::BadLen),
+        }
     }
 }
 
